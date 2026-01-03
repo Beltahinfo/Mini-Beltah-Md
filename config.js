95cd27a299423dc84cff09f769bbf224c2ef1e00
Beltahinfo <beltahhub@gmail.com>
2026-01-03 14:13:53 +0300

    Update Heroku URL and group invite link in config

--- config.js
+++ config.js
@@ -13,17 +13,17 @@ module.exports = {
     AUTO_VOICE: 'true',
     AUTO_LIKE_STATUS: 'true',
     AUTO_RECORDING: 'true',
-    HEROKU_APP_URL: 'https://silva-mini-bob-xmd.onrender.com/',
+    HEROKU_APP_URL: 'https://mini-beltah-md.onrender.com/',
     AUTO_LIKE_EMOJI: ['💥', '👍', '😍', '💗', '🎈', '🎉', '🥳', '😎', '🚀', '🔥'],
     PREFIX: '.',
     MAX_RETRIES: 3,
-    GROUP_INVITE_LINK: 'https://chat.whatsapp.com/J1h8UQencpe7wTwvS7hHxj',
+    GROUP_INVITE_LINK: 'https://chat.whatsapp.com/E6iKCw5jzpoGwD3lOZu6l2',
     ADMIN_LIST_PATH: './lib/admin.json',
     RCD_IMAGE_PATH: 'https://files.catbox.moe/9l9k6x.jpg',
     NEWSLETTER_JID: '120363200367779016@newsletter',
     NEWSLETTER_MESSAGE_ID: '428',
     OTP_EXPIRY: 300000,    OWNER_NUMBER: '254114141192',
-    CHANNEL_LINK: 'https://whatsapp.com/channel/0029VaAkETLLY6d8qhLmZt2v',
+    CHANNEL_LINK: 'https://whatsapp.com/channel/0029Vb7MQIbJUM2bMMV5PM1O',
     GIST_URL: 'https://gist.githubusercontent.com/SilvaTechDev/8fdbbfb81639750b3170dc00103272c2/raw'
 };
 

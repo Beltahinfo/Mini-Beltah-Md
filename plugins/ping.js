e01c268fa757343269d32a869f6af522e4a93a29
Beltahinfo <beltahhub@gmail.com>
2026-01-03 14:11:23 +0300

    Update ping command response message

--- plugins/ping.js
+++ plugins/ping.js
@@ -2,13 +2,14 @@ module.exports = {
   command: "ping",
   desc: "Check bot response time",
   category: "utility",
+  react: '♻️',
   use: ".ping",
   fromMe: false,
   filename: __filename,
 
   execute: async (sock, msg) => {
     const start = Date.now();
-    await sock.sendMessage(msg.key.remoteJid, { text: "*silva speed...*" });
+    await sock.sendMessage(msg.key.remoteJid, { text: "*BELTAH-MD Speed Test...*" });
     const latency = Date.now() - start;
     
     await sock.sendMessage(msg.key.remoteJid, { 
